c0f98cd69dbb2b91f296602583906ca3298c6c0e
Redger1 <artyom-sh2001@yandex.ru>
2025-07-16 16:12:29 +0300

    feat: инициализировать проект, подключить бибилиотеку

--- GitHubRepos-UI/ViewController.swift
+++ GitHubRepos-UI/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import GithubAPI
+import Combine
 
 class ViewController: UIViewController {
-
+    private var viewModel: SearchViewModelProtocol
+    private var cancellables = Set<AnyCancellable>()
+ 
+    init(viewModel: SearchViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    require init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var label: UILabel {
+        let newLabel = UILabel()
+        newLabel.text = "Repos count:"
+        return newLabel
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+        view.addSubview(label)
+        
+        viewModel.search(query: "swift")
+        
+        viewModel.reposPublisher
+            .receive(on: RunLoop.main)
+            .sink { [weak self] repos in
+                guard let self else { return }
+                self.label.text = "Repos count: \(repos.count)"
+            }
+            .store(in: &cancellables)
     }
-
-
 }
 
